f139f2529b123407992bff344b4327a4faf7670f
maxime42 <maxime@wigo-media.com>
2020-04-27 10:01:27 +0200

    build(locaw) : npm , webpack

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import moment from 'moment'   
+import './style.css';
+
+
+//Animation Placeholder input
+var input = document.querySelectorAll('input');
+var showMp = document.querySelectorAll('.show-mp');
+
+//__Animation Placeholder input
+for (var i = 0; i < input.length; i++) {
+    input[i].addEventListener('focus', function(event) {
+        event.target.classList.add('active');
+    });
+    input[i].addEventListener('blur', function(event) {
+    	if (event.target.value == "") {
+    		event.target.classList.remove('active');
+    	}
+    });
+}
+
+for (var i = 0; i < showMp.length; i++) {
+    showMp[i].addEventListener('click', function(event) {
+        if (event.target.nextSibling.nextSibling.getAttribute('type') == "password") {
+        	document.querySelector('#show-mp-1').classList.add('active')
+        	document.querySelector('#show-mp-2').classList.remove('active')
+        	event.target.nextSibling.nextSibling.setAttribute('type', 'text');
+        }else{
+        	document.querySelector('#show-mp-2').classList.add('active')
+        	document.querySelector('#show-mp-1').classList.remove('active')
+        	event.target.nextSibling.nextSibling.setAttribute('type', 'password');
+        }
+        
+    });
+}
+
+
+//Exercice moment
+
+moment.locale('fr')  
+
+const element = document.createElement('p'); 
+
+element.innerHTML =  
+`Le cours a commencé :  
+${moment("2020 04 27, 8:00:00 am", "YYYY MM DD, h:mm:ss a")   
+	.fromNow()}`; 
+
+document.body.appendChild(element);
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');  
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {   
+	entry: './src/index.js',   
+	output: {     
+		filename: 'bundle.js',     
+		path: path.resolve(__dirname, 'dist'),   
+	}, 
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+				  MiniCssExtractPlugin.loader,
+				  'css-loader',
+				],
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+					use: [
+						'file-loader',
+					],
+				},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					'file-loader',
+				],
+			},
+			{
+		        test: /\.html$/i,
+		        loader: 'html-loader',
+		    },
+		],
+	},
+	plugins: [    
+		new HtmlWebpackPlugin({     
+			template: './src/index.html',     
+			inject: true,     
+			filename: 'index.html'   
+		}),   
+		new MiniCssExtractPlugin() 
+	], 
+	devServer: {   
+		contentBase: path.join(__dirname, 'dist'), 
+	},
+};
